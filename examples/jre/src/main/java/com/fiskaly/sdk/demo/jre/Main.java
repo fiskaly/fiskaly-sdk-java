a626e5e7790856ea1e147c6a3cc1898b89622bd6
Angela Brett <angela.brett@fiskaly.com>
2021-08-03 14:48:13 +0200

    Added back v1 code in JRE example, and extended it to complete a transaction Fixed some JSON formatting in the v2 examples (still not tested)

--- examples/jre/src/main/java/com/fiskaly/sdk/demo/jre/Main.java
+++ examples/jre/src/main/java/com/fiskaly/sdk/demo/jre/Main.java
@@ -2,17 +2,17 @@ package com.fiskaly.sdk.demo.jre;
 
 import com.fiskaly.sdk.*;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.UUID;
 
 import com.fiskaly.sdk.factories.GsonFactory;
 import com.google.gson.Gson;
-import net.iharder.Base64;
 
 public class Main {
   static String tssUUID = "";
   static String clientUUID = "";
-  static String adminPuk = "";
+  static String adminPUK = "";
   static FiskalyHttpClient client = null;
   static String adminPIN = "0123456789";
   static String transactionUUID = "";
@@ -23,10 +23,19 @@ public class Main {
     final String apiKey = System.getenv("FISKALY_API_KEY");
     final String apiSecret = System.getenv("FISKALY_API_SECRET");
     client =
-        new FiskalyHttpClient(apiKey, apiSecret, "https://kassensichv.fiskaly.dev/api/v2", "https://kassensichv-middleware.fiskaly.dev");
-    final FiskalyHttpResponse response = client.request("GET", "/tss");
-    System.out.println("List TSS response:");
-    System.out.println(response);
+            new FiskalyHttpClient(apiKey, apiSecret, "https://kassensichv.io/api/v1");
+    listTSS();
+    createTSSV1();
+    createClient();
+    createTransactionV1();
+    finishTransactionV1();
+
+      //now do something similar on v2
+    final String apiKeyv2 = System.getenv("FISKALY_API_KEY_V2");
+    final String apiSecretv2 = System.getenv("FISKALY_API_SECRET_V2");
+    client =
+        new FiskalyHttpClient(apiKeyv2, apiSecretv2, "https://kassensichv.fiskaly.dev/api/v2", "https://kassensichv-middleware.fiskaly.dev");
+    listTSS();
     createTSS();
     personalizeTSS();
     changeAdminPIN();
@@ -40,6 +49,12 @@ public class Main {
     disableTSS();
   }
 
+  private static void listTSS() throws IOException, FiskalyHttpException, FiskalyClientException, FiskalyHttpTimeoutException {
+    final FiskalyHttpResponse response = client.request("GET", "/tss");
+    System.out.println("List TSS response:");
+    System.out.println(response);
+  }
+
   public static void createTSS() throws Exception {
     UUID uuid = UUID.randomUUID();
     tssUUID = uuid.toString();
@@ -49,7 +64,7 @@ public class Main {
     final String decodedBody = new String(response.body);
     final CreateTSSResponse body = GSON.fromJson(decodedBody, CreateTSSResponse.class);
     System.out.println("admin puk = "+body.admin_puk);
-    adminPuk = body.admin_puk;
+    adminPUK = body.admin_puk;
   }
 
   public static void setTSSState(String state) throws Exception {
@@ -66,7 +81,7 @@ public class Main {
   }
 
   public static void changeAdminPIN() throws Exception {
-    final String body = "{ \"admin_puk\" : \""+adminPuk+"\", \"new_admin_pin\" : \""+adminPIN+"\" }";
+    final String body = "{ \"admin_puk\" : \""+ adminPUK +"\", \"new_admin_pin\" : \""+adminPIN+"\" }";
     System.out.println("Setting admin PIN to "+adminPIN);
     System.out.println(body);
     final FiskalyHttpResponse response = client.request("PATCH", "/tss/" + tssUUID + "/admin", body.getBytes());
@@ -133,16 +148,16 @@ public class Main {
             "                    \"receipt\": {\n" +
             "                        \"receipt_type\": \"RECEIPT\",\n" +
             "                        \"amounts_per_vat_rate\": {\n" +
-            "                            {\n" +
+            "                            [\n" +
             "                                \"vat_rate\": \"NORMAL\",\n" +
             "                                \"amount\": \"21.42\"\n" +
-            "                            }\n" +
+            "                            ]\n" +
             "                        },\n" +
             "                        \"amounts_per_payment_type\": {\n" +
-            "                            {\n" +
+            "                            [\n" +
             "                                \"payment_type\": \"NON_CASH\",\n" +
             "                                \"amount\": \"21.42\"\n" +
-            "                            }\n" +
+            "                            ]\n" +
             "                        }\n" +
             "                    }\n" +
             "                }\n" +
@@ -164,16 +179,16 @@ public class Main {
             "                    \"receipt\": {\n" +
             "                        \"receipt_type\": \"RECEIPT\",\n" +
             "                        \"amounts_per_vat_rate\": {\n" +
-            "                            {\n" +
+            "                            [\n" +
             "                                \"vat_rate\": \"NORMAL\",\n" +
             "                                \"amount\": \"21.42\"\n" +
-            "                            }\n" +
+            "                            ]\n" +
             "                        },\n" +
             "                        \"amounts_per_payment_type\": {\n" +
-            "                            {\n" +
+            "                            [\n" +
             "                                \"payment_type\": \"NON_CASH\",\n" +
             "                                \"amount\": \"21.42\"\n" +
-            "                            }\n" +
+            "                            ]\n" +
             "                        }\n" +
             "                    }\n" +
             "                }\n" +
@@ -190,6 +205,59 @@ public class Main {
 
   public static void disableTSS() throws Exception {
     setTSSState("DISABLED");
+  }
+
+  ///V1 versions
+  public static void createTSSV1() throws Exception {
+    UUID uuid = UUID.randomUUID();
+    tssUUID = uuid.toString();
+    final String body = "{\n" +
+            "            \"description\": \"JRE Test TSS\",\n" +
+            "            \"state\": \"INITIALIZED\"\n" +
+            "        }";
+    final FiskalyHttpResponse response = client.request("PUT", "/tss/" + tssUUID, body.getBytes());
+    System.out.println("Create TSS response:");
+    System.out.println(response);
+  }
+
+  public static void createTransactionV1() throws Exception {
+    UUID uuid = UUID.randomUUID();
+    transactionUUID = uuid.toString();
+    final String body = "{\"state\": \"ACTIVE\",\n" +
+            "            \"client_id\": \"" + clientUUID +"\"\n" +
+            "        }";
+    System.out.println("Creating V1 transaction");
+    System.out.println(body);
+    transactionRevision = 0;
+    final FiskalyHttpResponse response = client.request("PUT", "/tss/" + tssUUID + "/tx/" + transactionUUID, body.getBytes());
+
+    System.out.println("Create V1 Transaction response:");
+    System.out.println(response);
+  }
+
+  public static void finishTransactionV1() throws Exception {
+    final String body = "{\n" +
+            "            \"state\": \"FINISHED\",\n" +
+            "            \"client_id\": \""+clientUUID+"\",\n" +
+            "            \"schema\": {\n" +
+            "                \"standard_v1\": {\n" +
+            "                    \"receipt\": {\n" +
+            "                        \"receipt_type\": \"RECEIPT\",\n" +
+            "                        \"amounts_per_vat_rate\": [\n" +
+            "                            {\"vat_rate\": \"19\", \"amount\": \"14.28\"}\n" +
+            "                        ],\n" +
+            "                        \"amounts_per_payment_type\": [\n" +
+            "                            {\"payment_type\": \"NON_CASH\", \"amount\": \"14.28\"}\n" +
+            "                        ]\n" +
+            "                    }\n" +
+            "                }\n" +
+            "            }\n" +
+            "        }";
+    System.out.println("Finishing transaction");
+    System.out.println(body);
+    final FiskalyHttpResponse response = client.request("PUT", "/tss/" + tssUUID + "/tx/" + transactionUUID, body.getBytes(), Collections.singletonMap("last_revision", 1));
+    System.out.println("Finish Transaction response:");
+    System.out.println(response);
   }
 
 }
